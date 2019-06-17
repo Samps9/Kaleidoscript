62b7bc6a736bd04900eaeb7e14847b4ea621a2e8
Sam Lubin <samlubin@yahoo.com>
2019-06-17 07:57:47 -0400

    fixes #1

--- kaleidoscript.js
+++ kaleidoscript.js
@@ -1,34 +1,39 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
 
     class Kaleidoscript {
 
-    	run(text){
+    	run(text) {
     		this.renderResponse(this.buildResponse(this.scramble(text)));
     	}
 
-    	renderResponse(elem){
+    	renderResponse(elem) {
     		const responses = document.getElementById("responses");
     		responses.appendChild(elem);
     		responses.scrollTo(0, responses.scrollHeight);
     	}
 
-    	buildResponse(text){
+    	buildResponse(text) {
     		let result = document.createElement("div");
     		result.setAttribute("class", "response border pad bg-vintage inconsolata");
     		result.innerText = text;
     		return result;
     	}
 
-		scramble(text){
+		scramble(text) {
 			let result = "";
-			text = text.split(" ");
-			while(text.length > 0) {
-				let wordIndex = Math.floor(Math.random() * Math.floor(text.length));
-				let word = text[wordIndex];
-				result += word + " ";
-				text.splice(wordIndex, 1);
-			} 
+			if (text.length == 0) {
+    			console.log("KAW")
+    			result = "Thank you for showing your curiosity! Try adding and submitting text to the input field for more interesting results..."
+    		} else {
+				text = text.split(" ");
+				while(text.length > 0) {
+					let wordIndex = Math.floor(Math.random() * Math.floor(text.length));
+					let word = text[wordIndex];
+					result += word + " ";
+					text.splice(wordIndex, 1);
+				} 
+			}
 			return result;
 		}
 	}
@@ -36,9 +41,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
 	const instance = new Kaleidoscript();
 	const submit = document.getElementById("submit-button");
 	const input = document.getElementById("input-field");
+
 	submit.addEventListener("mouseup", (text) => {instance.run(input.value)});
 	input.addEventListener("keypress", (event) => {
-		if(event.which == 13 && !event.shiftKey){
+		if(event.which == 13 && !event.shiftKey) {
 			event.preventDefault();
 			instance.run(input.value);
 		}	
